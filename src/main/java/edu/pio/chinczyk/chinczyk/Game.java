8897be396f267c333e0b1032df6e40760cab87a7
MikolajRajczyk <235958@edu.p.lodz.pl>
2022-05-17 18:23:29 +0200

    utworzenie sceny z zasadami gry

--- src/main/java/edu/pio/chinczyk/chinczyk/Game.java
+++ src/main/java/edu/pio/chinczyk/chinczyk/Game.java
@@ -1,18 +1,18 @@
 package edu.pio.chinczyk.chinczyk;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Game extends javafx.application.Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Game.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("CHINCZYK!");
         stage.setScene(scene);
         stage.show();
     }
