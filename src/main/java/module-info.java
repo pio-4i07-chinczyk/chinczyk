e9eb7799827e4be9811e937dd13b5c36b60c8c61
Patryk Klimas <patrykbajos@protonmail.ch>
2022-06-01 21:00:26 +0200

    dodany eksport edu.pio.chinczyk.game do uzycia przez JaveFX

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module edu.pio.chinczyk {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens edu.pio.chinczyk to javafx.fxml;
     exports edu.pio.chinczyk;
+
+    exports edu.pio.chinczyk.game;
 }
